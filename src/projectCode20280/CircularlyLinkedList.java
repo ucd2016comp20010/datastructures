689098af46ae446e26327e74aa0415469f221e7f
aonghus <aonghuslawlor@gmail.com>
2020-01-26 16:47:41 +0000

    DLL and CLL lists

--- src/projectCode20280/CircularlyLinkedList.java
+++ src/projectCode20280/CircularlyLinkedList.java
@@ -2,12 +2,18 @@ package projectCode20280;
 
 import java.util.Iterator;
 
-public class SinglyLinkedList<E> implements List<E> {
+public class CircularlyLinkedList<E> implements List<E> {
 
 	private class Node<E> {
-		/// TODO
+		
 	}
 	
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 	@Override
 	public boolean isEmpty() {
 		// TODO Auto-generated method stub
@@ -33,33 +39,6 @@ public class SinglyLinkedList<E> implements List<E> {
 	}
 
 	@Override
-	public Iterator<E> iterator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public int size() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-	public static void main(String[] args) {
-		String[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-
-		SinglyLinkedList<String> sll = new SinglyLinkedList<String>();
-		for (String s : alphabet) {
-			sll.addFirst(s);
-			sll.addLast(s);
-		}
-		System.out.println(sll.toString());
-
-		for (String s : sll) {
-			System.out.print(s + ", ");
-		}
-	}
-
-	@Override
 	public E removeFirst() {
 		// TODO Auto-generated method stub
 		return null;
@@ -70,4 +49,16 @@ public class SinglyLinkedList<E> implements List<E> {
 		// TODO Auto-generated method stub
 		return null;
 	}
+
+	@Override
+	public Iterator<E> iterator() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
