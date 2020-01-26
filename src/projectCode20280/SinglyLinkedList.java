bb47b5e4c1a2815bc2aaff97cacd1447b1d384de
aonghus <aonghuslawlor@gmail.com>
2020-01-26 16:27:23 +0000

    size method

--- src/projectCode20280/SinglyLinkedList.java
+++ src/projectCode20280/SinglyLinkedList.java
@@ -2,6 +2,10 @@ package projectCode20280;
 
 public class SinglyLinkedList {
 
+	public int size() {
+		return 0;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("SinglyLinkedList");
