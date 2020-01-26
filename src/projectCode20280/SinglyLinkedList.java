9ddaa399c86ec0698b1c11fb39068484647f5afe
aonghus <aonghuslawlor@gmail.com>
2020-01-26 16:07:58 +0000

    first commit

---
+++ src/projectCode20280/SinglyLinkedList.java
@@ -0,0 +1,10 @@
+package projectCode20280;
+
+public class SinglyLinkedList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("SinglyLinkedList");
+	}
+
+}
